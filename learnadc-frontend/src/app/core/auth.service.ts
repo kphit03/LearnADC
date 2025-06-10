d4da610436c3a16c029eccd0b03a28d4ad4f2887
kphitsan <kevinphitsanu22@gmail.com>
2025-06-10 15:13:12 -0500

    Deployed backend, updated frontend apiurl.

--- learnadc-frontend/src/app/core/auth.service.ts
+++ learnadc-frontend/src/app/core/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
--- learnadc-frontend/src/environments/environment.prod.ts
+++ learnadc-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://placeholderurl.com' 
+  apiBaseUrl: 'http://13.221.93.180:8080/' 
 };
