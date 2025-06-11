c0f7d6c5a161b04aa73ee885ecdbbf7910255a02
kphitsan <kevinphitsanu22@gmail.com>
2025-06-10 19:15:16 -0500

    Debuggin contact form

--- learnadc-frontend/src/app/core/contact.service.ts
+++ learnadc-frontend/src/app/core/contact.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment.prod';
 import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
 
 export interface ContactRequest { //structure of how we are sending the data (matches backend DTO)
   name: string;
@@ -20,7 +21,19 @@ export class ContactService {
 
   constructor(private http: HttpClient) { }
 
+  // sendContactForm(data: ContactRequest): Observable<string> {
+  //   return this.http.post(this.apiUrl, data, { responseType: 'text' }); //responseType is the response we expect from the backend.
+  // }
   sendContactForm(data: ContactRequest): Observable<string> {
-    return this.http.post(this.apiUrl, data, { responseType: 'text' }); //responseType is the response we expect from the backend.
-  }
+  const headers = new HttpHeaders({
+    'Content-Type': 'application/json'
+    // 👈 Intentionally omitting Authorization
+  });
+
+  return this.http.post(this.apiUrl, data, {
+    headers,
+    responseType: 'text'
+  });
+}
+
 }
